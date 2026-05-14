0a572c2b8822d78ae00a68ced97cf10f1360f508
Nuri Yuri <yuri@pokemonworkshop.com>
2026-05-14 11:43:35 +0200

    Fix sub page index issues

--- src/getPageEntry.ts
+++ src/getPageEntry.ts
@@ -1,6 +1,21 @@
 import { getEntry, getCollection } from 'astro:content';
 import { sortPages } from './sortPages';
 
+const isAValidSubPage = (id: string, entryId: string, subPageSlugPrefix: string, locale: string) => {
+  if (!id.startsWith(subPageSlugPrefix)) return false;
+  if (!id.endsWith(locale)) return false;
+  if (entryId === id) return false;
+
+  const slugSuffix = id.replace(subPageSlugPrefix,'');
+  if (!slugSuffix.includes('/')) return true;
+
+  if (slugSuffix.endsWith(`/index_${locale}`)) {
+    return slugSuffix.split('/').length === 2
+  }
+
+  return false;
+}
+
 export const getPageEntry = async (slugParam: string, locale: 'en' | 'fr', noSubPage?: boolean) => {
   const [slug, tag] = (slugParam ?? 'index').split('_');
   const options = [`${slug}_${locale}`, `${slug}/index_${locale}`];
@@ -9,7 +24,7 @@ export const getPageEntry = async (slugParam: string, locale: 'en' | 'fr', noSub
 
   if (!noSubPage && entry.id.endsWith(`/index_${locale}`)) {
     const subPageSlugPrefix = `${slug}/`;
-    const subPages = await getCollection('pages', (e) => e.id.startsWith(subPageSlugPrefix) && e.id.endsWith(locale) && e.id !== entry.id);
+    const subPages = await getCollection('pages', (e) => isAValidSubPage(e.id, entry.id, subPageSlugPrefix, locale));
     if (!tag) return { entry, subPages: sortPages(subPages).map((e) => e.id), tag };
 
     const subPagesByTag = subPages.filter((e) => e.data.tags?.includes(tag));
